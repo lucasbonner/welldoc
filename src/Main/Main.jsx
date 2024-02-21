6fd31f2d7dfe9474ed130ed068177d69357ba77b
lucasbonner <lucasbonner@protonmail.com>
2024-02-21 17:08:52 -0600

    Adding next section

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -3,6 +3,13 @@ import './Main.scss'
 
 export default function Main() {
     return (
-        <h1>Main</h1>
+        <div className="main-holder">
+            <h2 className="text-tag">Don't Mask Your Chronic Conditions. Treat the Cause.</h2>
+            <p>
+            The doctors at Personalized Wellness Clinic dig deep to discover why you feel the way you do.
+
+            Call us to schedule an appointment if you suffer from any of the following chronic conditions and you want to address the underlying issues.
+            </p>
+        </div>
     )
 }
