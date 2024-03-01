5f591a0e477d17822ccc0cdab36a2f700317bf73
lucasbonner <lucasbonner@protonmail.com>
2024-02-29 18:05:44 -0600

    Small additions

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -5,11 +5,25 @@ export default function Main() {
     return (
         <div className="main-holder">
             <h2 className="text-tag">Don't Mask Your Chronic Conditions. Treat the Cause.</h2>
-            <p>
-            The doctors at Personalized Wellness Clinic dig deep to discover why you feel the way you do.
+            <div className="paragraph-holder">
+                <p className="chronic-paragraph">
+                The doctors at Personalized Wellness Clinic dig deep to discover why you feel the way you do.
+                </p>
+                <p className="chronic-paragraph">
+                Call us to schedule an appointment if you suffer from any of the following chronic conditions and you want to address the underlying issues.
+                </p>
+            </div>
+            <h2 className="text-tag">Your health. Your Way</h2>
+            <div className="paragraph-holder">
+                <p className="chronic-paragraph">
+                Your care begins with an extended consultation with Dr. Bonner, during which samples will be taken for lab work, and you will discuss your medical history, symptoms, goals, and expectations.  Dr. Bonner will then recommend which Personalized Wellness program best suits your needs:
+                </p>
+                <p className="chronic-paragraph">
+                Take Your First Step To Feeling Well Again
+                Your Best Health at Any Age
 
-            Call us to schedule an appointment if you suffer from any of the following chronic conditions and you want to address the underlying issues.
-            </p>
+                </p>
+            </div>
         </div>
     )
 }
